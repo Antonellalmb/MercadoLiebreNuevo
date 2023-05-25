a4c7fbbf4cc87451dbd59e6787e052a35e9701d2
Antonellalmb <3lmbmartinez@gmail.com>
2023-05-24 22:27:54 -0300

    arreglo

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ const app = express();
 const path = require("path");
 
 app.use(express.static("public"));
-//const publicFolderPath = path.resolve(__dirname, "./public");
 
-//app.use(express.static(publicFolderPath));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor corriendo en el puerto 3000')
 });
 
-//app.get("/", (req, res) => {
- //   res.send("funcionando")
-//});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
