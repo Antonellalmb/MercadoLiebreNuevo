28a90b253d8e2797a44a655256c26ac02fb3275e
Antonellalmb <3lmbmartinez@gmail.com>
2023-05-24 21:05:45 -0300

    cambiando json

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ app.use(express.static("public"));
 //const publicFolderPath = path.resolve(__dirname, "./public");
 
 //app.use(express.static(publicFolderPath));
-app.listen(3000, () => {
-  console.log("servidor corriendo en puerto 3000")
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Servidor corriendo en el puerto 3000')
 });
 
 //app.get("/", (req, res) => {
