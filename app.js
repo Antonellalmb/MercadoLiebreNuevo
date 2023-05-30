c747c96c85179357310c98d0f6f9f00d0eee75ad
Antonellalmb <3lmbmartinez@gmail.com>
2023-05-30 15:44:15 -0300

    cambiando referencias registros en app

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
 });
 
-app.get("/register.html", (req, res) => {
+app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname,"/views/register.html"))
 })
 
-app.get("/login.html", (req, res) => {
+app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname,"/views/login.html"))
 });
 
